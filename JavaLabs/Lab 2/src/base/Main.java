582cb92ae4d85873d5796d642ca1b2871adad2b4
gioisles <giovanni.raia1@marist.edu>
2022-01-27 19:24:51 -0500

    Lab2

--- JavaLabs/Lab 2/src/base/Main.java
+++ JavaLabs/Lab 2/src/base/Main.java
@@ -14,10 +14,16 @@ public class Main {
      */
     public static void main(String[] args) {
         // Make an if statement that triggers a print or println statement
+        
         // Make an if, else statement where the else statement triggers a print or println statement
+
         // Make an if, else if, else statement where the else if statement triggers a print or println statement
+
         // Make 2 variables and use them in an if else conditional print from one of the sections
+
         // Make an if statement using 2 variables and an AND(&&) statement that triggers a print or println statement
+
         // Make an if statement using 2 variables and an OR(||) statement that triggers a print or println statement
+
     }
 }
