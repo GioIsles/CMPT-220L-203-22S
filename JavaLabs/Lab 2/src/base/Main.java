c9877c2d2266d6d03dc250c0771cd79cb7d954aa
gioisles <giovanni.raia1@marist.edu>
2022-01-28 12:02:19 -0500

    Lab2 Main

--- JavaLabs/Lab 2/src/base/Main.java
+++ JavaLabs/Lab 2/src/base/Main.java
@@ -13,17 +13,47 @@ public class Main {
      *     - Else If
      */
     public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        boolean condition = true;
         // Make an if statement that triggers a print or println statement
-        
+        if(condition == true){
+            System.out.println("Hello World!");
+        }
         // Make an if, else statement where the else statement triggers a print or println statement
-
+        if(condition == true){
+            System.out.println("Welcome to the internet.");
+        }
+        else{
+            System.out.println("Sorry, it seems the computer isnt working.");
+        }
         // Make an if, else if, else statement where the else if statement triggers a print or println statement
-
+        if(x < 10){
+            System.out.println("X is less than 10.");
+        }
+        if else(x == 10){
+            System.out.println("X is equal to 10.");
+        }
+        else{
+            System.out.println("X is greater than 10.");
+        }
         // Make 2 variables and use them in an if else conditional print from one of the sections
-
+        if(x > y){
+            System.out.println("X is greater than Y.");
+        }
+        if else(x < y){
+            System.out.println("X is less than Y.");
+        }
+        else{
+            System.out.println("X is equal to Y.");
+        }
         // Make an if statement using 2 variables and an AND(&&) statement that triggers a print or println statement
-
+        if((x && y) > 5){
+            System.out.println("X and Y are greater than 5.");
+        }
         // Make an if statement using 2 variables and an OR(||) statement that triggers a print or println statement
-
+        if((x || y) > 15){
+            System.out.println("Either X or Y is greater than 15.");
+        }
     }
 }
