f5ac6a0b87b1f651d8fddd09590b7274ddc56173
gioisles <giovanni.raia1@marist.edu>
2022-02-03 17:01:51 -0500

    lab2 changes

--- JavaLabs/Lab 2/src/base/Main.java
+++ JavaLabs/Lab 2/src/base/Main.java
@@ -15,13 +15,12 @@ public class Main {
     public static void main(String[] args) {
         int x = 10;
         int y = 20;
-        boolean condition = true;
         // Make an if statement that triggers a print or println statement
-        if(condition == true){
+        if(x == 10){
             System.out.println("Hello World!");
         }
         // Make an if, else statement where the else statement triggers a print or println statement
-        if(condition == true){
+        if(y == 20){
             System.out.println("Welcome to the internet.");
         }
         else{
@@ -31,7 +30,7 @@ public class Main {
         if(x < 10){
             System.out.println("X is less than 10.");
         }
-        if else(x == 10){
+        else if(x == 10){
             System.out.println("X is equal to 10.");
         }
         else{
@@ -41,18 +40,18 @@ public class Main {
         if(x > y){
             System.out.println("X is greater than Y.");
         }
-        if else(x < y){
+        else if(x < y){
             System.out.println("X is less than Y.");
         }
         else{
             System.out.println("X is equal to Y.");
         }
         // Make an if statement using 2 variables and an AND(&&) statement that triggers a print or println statement
-        if((x && y) > 5){
+        if((x > 5) && (y > 5)){
             System.out.println("X and Y are greater than 5.");
         }
         // Make an if statement using 2 variables and an OR(||) statement that triggers a print or println statement
-        if((x || y) > 15){
+        if((x > 15) || (y > 15)){
             System.out.println("Either X or Y is greater than 15.");
         }
     }
