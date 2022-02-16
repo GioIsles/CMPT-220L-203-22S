1b4159a9b9e8eac1a0a6743a8c7b4080ae021ce8
gioisles <giovanni.raia1@marist.edu>
2022-02-16 16:19:23 -0500

    Lab4 updated

--- JavaLabs/Lab 4/src/base/Main.java
+++ JavaLabs/Lab 4/src/base/Main.java
@@ -1,4 +1,5 @@
 package base;
+import java.util.Scanner;
 
 public class Main {
     /**
@@ -15,30 +16,68 @@ public class Main {
      * - Structuring solutions
      */
     public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 3;
+        int[] nums = {1,2,3};
+
         // Give me an example of you using switch case.
+        switch(num1){
+            case 1:
+            System.out.println("The number is 1");
+            break;
+            case 2:
+            System.out.println("The number is 2");
+            break;
+            case 3:
+            System.out.println("The number is 3");
+            break;
+        }
         // Give me an example of you using a for loop
+        for(int i = 0; i < 5; i++){
+            System.out.println(nums);
+        }
         // Give me an example of you using a while loop
+        while(num2 < 4){
+            System.out.println(num2);
+            num2++;
+        }
     }
     // For the following todos you'll probably want to define them below and then call them from the main function above.
-
     /**
      * I want you to write a function that will take in a number and raise it to the power given.
      * For example if given the numbers 2 and 3. The math that the function should do is 2^3
      * and should print out or return 8. Print the output.
      */
-
+    int power(int num1, int num2){
+        return num1 ^ num2;
+    }
     /**
      * I want you to write a function that will take in a list and see how many times a given number is in the list.
      * For example if the array given is [2,3,5,2,3,6,7,8,2] and the number given is 2 the function should print out
      * or return 3. Print the output.
      */
-
+    int findList(int num1, int[] nums){
+        int numinlist = 0;       
+        for (int i = 0; i < nums.length; i++) 
+           {
+               if (num1 == nums[i]) 
+               {              
+                  numinlist = numinlist + 1;
+               }
+           }
+    }
     /**
      * Create a function named summation that will take in an int[ ] and return an int.
      * I want the function to then go through the given array and sum up digits in the array.
      * Once that’s done print or return the answer. Print the output
      */
-
+    int summation(int[] nums){
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++)
+        {
+            sum = sum + nums[i];
+        }
+    }
     /**
      * Give me a function that gives the answer to the pythagorean theorem.
      * I'd like you to reuse the exponent function from above as well as the functions below to make your function.
@@ -50,6 +89,13 @@ public class Main {
      *
      * Once that’s done print or return the answer. Print the output
      */
+    int pythagorean(int a, int b){
+        double a2 = Math.pow(a, 2);
+        double b2 = Math.pow(b, 2);
+        double sum = a2 + b2;
+        double c = Math.sqrt(sum);
+        return c;
+    }
 
     int addition(int a, int b) {
         int answer = a + b;
