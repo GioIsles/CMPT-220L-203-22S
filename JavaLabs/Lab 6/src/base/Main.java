be07165909fbc06d9a25570e2fce55312a1c0524
gioisles <giovanni.raia1@marist.edu>
2022-03-01 12:57:12 -0500

    Updated Lab6

--- JavaLabs/Lab 6/src/base/Main.java
+++ JavaLabs/Lab 6/src/base/Main.java
@@ -1,4 +1,7 @@
 package base;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 public class Main {
     /**
@@ -23,10 +26,36 @@ public class Main {
      */
     public static void main(String[] args) {
         // Create an Array, Populate it, and Print it out
+        int[] arr = {1,2,3,4};
+        System.out.println(arr);
         // Create an ArrayList, Populate it, and Print it out
+        ArrayList<Integer> arrList = new ArrayList<Integer>();
+        arrList.add(1);
+        arrList.add(2);
+        arrList.add(3);
+        System.out.println(arrList);
         // Create an LinkedList, Populate it, and Print it out
+        LinkedList<Integer> arrLink = new LinkedList<Integer>();
+        arrLink.add(1);
+        arrLink.add(2);
+        arrLink.add(3);
+        arrLink.add(4);
+        arrLink.add(5);
+        System.out.println(arrLink);
         // Create an Queue, Populate it, and Print it out
+        Queue<Integer> arrQueue = new LinkedList<Integer>();
+        arrQueue.add(2);
+        arrQueue.add(4);
+        arrQueue.add(6);
+        arrQueue.add(8);
+        System.out.println(arrQueue);
         // Create an Stack, Populate it, and Print it out
+        Stack<String> arrStack = new Stack<String>();
+        arrStack.push("One");
+        arrStack.push("Two");
+        arrStack.push("Three");
+        System.out.println(arrStack);
         // Using the information in the Lab 6 Jupyter Notebook create an instance of plant here.
+        
     }
 }
