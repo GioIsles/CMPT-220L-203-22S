59abda06f04f5a7de775f7082715b0dc5034344c
gioisles <giovanni.raia1@marist.edu>
2022-03-03 17:15:12 -0500

    Lab6 edit

--- JavaLabs/Lab 6/src/base/Main.java
+++ JavaLabs/Lab 6/src/base/Main.java
@@ -27,15 +27,20 @@ public class Main {
      */
     public static void main(String[] args) {
         // Create an Array, Populate it, and Print it out
+        System.out.println("Array:");
         int[] arr = {1,2,3,4};
         System.out.println(arr[1]);
+        System.out.println();
         // Create an ArrayList, Populate it, and Print it out
+        System.out.println("Array List:");
         ArrayList<Integer> arrList = new ArrayList<Integer>();
         arrList.add(1);
         arrList.add(2);
         arrList.add(3);
         System.out.println(arrList);
+        System.out.println();
         // Create an LinkedList, Populate it, and Print it out
+        System.out.println("Linked List:");
         LinkedList<Integer> arrLink = new LinkedList<Integer>();
         arrLink.add(1);
         arrLink.add(2);
@@ -43,6 +48,7 @@ public class Main {
         arrLink.add(4);
         arrLink.add(5);
         System.out.println(arrLink);
+        System.out.println();
         // Create an Queue, Populate it, and Print it out
         Queue<Integer> arrQueue = new LinkedList<Integer>();
         arrQueue.add(2);
@@ -50,25 +56,39 @@ public class Main {
         arrQueue.add(6);
         arrQueue.add(8);
         System.out.println(arrQueue);
+        System.out.println();
         // Create an Stack, Populate it, and Print it out
         Stack<String> arrs = new Stack<String>();
         arrs.push("One");
         arrs.push("Two");
         arrs.push("Three");
         System.out.println(arrs);
+        System.out.println();
         // Using the information in the Lab 6 Jupyter Notebook create an instance of plant here.
-        Plant plant1 = new Plant(needsWatering, plantFamily, name);
+        Plant plant1 = new Plant(needsWatering, plantFamily, name, var1, var2);
         plant1.needsWatering = true;
         plant1.plantFamily = "Asphodelaceae";
         plant1.name = "Aloe Vera";
+        plant1.var1 = 12;
+        plant1.var2 = 8;
         plant1.wateringInstructions();
         plant1.sunlightRequirements();
         plant1.PlantPrint();
+        System.out.println();
+        Plant plant2 = new Plant(needsWatering, plantFamily, name, var1, var2);
+        plant2.needsWatering = false;
+        plant2.plantFamily = "Cacti";
+        plant2.name = "Silver Torch Cactus";
+        plant2.var2 = 17;
+        plant2.sunlightRequirements();
+        plant2.PlantPrint();
     }
 
     public static boolean needsWatering;
     public static String plantFamily;
     public static String name;
+    public static int var1;
+    public static int var2;
 
     public void wateringInstructions() {
     }
--- JavaLabs/Lab 6/src/base/Plant.java
+++ JavaLabs/Lab 6/src/base/Plant.java
@@ -3,16 +3,19 @@ public class Plant {
     boolean needsWatering;
     String plantFamily;
     String name;
+    public int var1;
+    public int var2;
 
-    public Plant(boolean needsWatering2, String plantFamily2, String name2) {
+
+    public Plant(boolean needsWatering2, String plantFamily2, String name2, int var1, int var2) {
     }
 
     void wateringInstructions(){
-        System.out.println("Use 12oz of water.");
+        System.out.println("Use " + var1 + "oz of water.");
     }
 
     void sunlightRequirements(){
-        System.out.println("Get 8 hours of sun.");
+        System.out.println("Get " + var2 + " hours of sun.");
     }
 
     void PlantPrint(){
