8029cac42e8f41ae0612635e3c67c0590cf52600
gioisles <giovanni.raia1@marist.edu>
2022-05-03 23:13:23 -0400

    project

--- Project/Main.java
+++ Project/Main.java
@@ -1,4 +1,4 @@
-package project;
+package Project;
 
 class Node {
     int data;
@@ -16,22 +16,21 @@ class BST {
         return a;
     }
 
-    public Node insert(Node node, int val){
+    public Node insert(Node node, int x){
         if(node == null){
-            return createNewNode(val);
+            return createNewNode(x);
         }
 
-        if(val < node.data){
-            node.left = insert(node.left, val);
+        if(x < node.data){
+            node.left = insert(node.left, x);
         }
-        else if(val > node.data){
-            node.right = insert(node.right, val);
+        else if(x > node.data){
+            node.right = insert(node.right, x);
         }
-
         return node;
     }
 
-    public boolean findNode(Node node, int val){
+    public boolean findNode(Node node, int x){
         if (node == null){
             return false;
         }
@@ -39,19 +38,68 @@ class BST {
         boolean nodeFound = false;
 
         while(node != null){
-            if(val < node.data){
+            if(x < node.data){
                 node = node.left;
             }
-            else if(val > node.data){
+            else if(x > node.data){
                 node = node.right;
             }
             else{
                 nodeFound = true;
                 break;
             }
-            return nodeFound;
         }
+        return nodeFound;
     }
+
+    public int height(Node node){
+        if(node == null){
+            return 0;
+        }
+        return Math.max(height(node.left), height(node.right)) + 1;
+    }
+
+    public int minVal(Node node){
+        if(node == null){
+            System.out.println("N/A");
+            return 0;
+        }
+        while(node.left != null){
+            node = node.left;
+        }
+        return node.data;
+    }
+
+    public int maxVal(Node node){
+        if(node == null){
+            System.out.println("N/A");
+            return 0;
+        }
+        while(node.right != null){
+            node = node.right;
+        }
+        return node.data;
+    }
+    
+    static int x = 0;
+    public Node NthLargest(Node node, int n){
+        if(node == null){
+            return null;
+        }
+
+        Node right = NthLargest(node.right, n);
+
+        if(right != null){
+            return right;
+        }
+        x++;
+
+        if(x == n){
+            return node;
+        }
+        return NthLargest(node.left, n);
+    }
+
     public void inorder(Node node){
         if(node == null){
             return;
@@ -80,26 +128,97 @@ class BST {
         preorder(node.left);
         preorder(node.right);
     }
+
+    public Node delete(Node node, int x) {
+        if(node == null) {
+          return node;
+        }
+        
+        if(x < node.data) {
+          node.left = delete(node.left, x);
+        } 
+        else if(x > node.data) {
+          node.right = delete(node.right, x);
+        } 
+        else {
+          if(node.left == null || node.right == null) {
+            Node temp = node.left != null ? node.left : node.right;
+    
+            if(temp == null) {
+              return null;
+            } 
+            else {
+              return temp;
+            }
+          } 
+          else {
+            Node successor = get(node);
+            node.data = successor.data;
+            
+            node.right = delete(node.right, successor.data);
+            return node;
+          }
+        }
+        return node;
+      }
+      
+      public Node get(Node node) {
+        if(node == null) {
+          return null;
+        }
+        
+        Node temp = node.right;
+        
+        while(temp.left != null) {
+          temp = temp.left;
+        }
+        return temp;
+      }
 }
 
 public class Main {
     public static void main(String[] args) {
         BST a = new BST();
-        Node root = a.createNewNode(2);
+        Node root = a.createNewNode(1);
 
-        root = a.insert(root, 8);
-        root = a.insert(root, 3);
-        root = a.insert(root, 6);
-        root = a.insert(root, 10);
+        root = a.insert(root, 9);
         root = a.insert(root, 4);
         root = a.insert(root, 7);
-        root = a.insert(root, 1);
-        root = a.insert(root, 14);
         root = a.insert(root, 13);
-    
+        root = a.insert(root, 6);
+        root = a.insert(root, 7);
+        root = a.insert(root, 8);
+        root = a.insert(root, 17);
+        root = a.insert(root, 16);
+        root = a.insert(root, 1);
+
+        System.out.println("In Order:");
         a.inorder(root);
+        System.out.println("");
+
+        System.out.println("Post Order:");
         a.postorder(root);
+        System.out.println("");
+        
+        System.out.println("Pre Order");
         a.preorder(root);
+        System.out.println("");
+
+        System.out.println("Min Value");
+        a.minVal(root);
+        System.out.println("");
+
+        System.out.println("Max Value");
+        a.maxVal(root);
+        System.out.println("");
+
+        System.out.println("Nth Value");
+        a.NthLargest(root, 2);
+        System.out.println("");
+
+        System.out.println("Delete + Reorganise");
+        a.delete(root, 13);
+        a.inorder(root);
+        System.out.println("");
     }
 }
-
